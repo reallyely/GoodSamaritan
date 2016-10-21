3577f0461e426364fb641454a482967a549e6d33
Stephen Ely <sely@pcgus.com>
2016-10-21 17:01:49 -0400

    Adds Font Awesome and close button to CTA

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -4,6 +4,7 @@ import TransitionGroup from 'react-addons-transition-group'
 import anime from 'animejs';
 import {findDOMNode} from 'react-dom';
 import TweenMax from 'gsap';
+import FontAwesome from 'react-fontawesome';
 
 import './CallToAction.css'
 
@@ -13,6 +14,14 @@ const style = {
 		fontFamily: 'IM Fell French Canon SC, serif',
 	},
 	button: {
+		backgroundImage: `
+			linear-gradient(0deg, rgb(201, 46, 46),
+			rgb(249, 57, 57))
+		`,
+		borderRadius: '5px',
+		border: 'outset 2px white'
+	},
+	expanded: {
 		backgroundImage: `
 			linear-gradient(0deg, rgb(201, 46, 46),
 			rgb(249, 57, 57))
@@ -37,10 +46,9 @@ class CallToAction extends React.Component {
 
 	handleClick() {
 		this.setState({
-			expanded: true,
+			expanded: !this.state.expanded,
 		})
 	}
-
 
 	handleMouseIn() {
 		this.setState({hover:true})
@@ -58,16 +66,21 @@ class CallToAction extends React.Component {
 				<TransitionGroup>
 					{
 						this.state.expanded
-							? <div> IM HERE! </div>
-							: <Collapsed
+							? <Expanded key={1} p={2}
+									onClose={this.handleClick}
+									style={style.expanded}
+								/>
+							: this.state.complete ?
+								null
+							: <Collapsed key={2}
 									p={2}
-									 onMouseEnter={this.handleMouseIn}
-									 onMouseLeave={this.handleMouseOut}
-									 onClick={this.handleClick}
-									 style={style.button}
-									 justify="flex-start"
-									 className={this.state.hover ? 'hover' : 'hoverOut'}
-								 />
+									onMouseEnter={this.handleMouseIn}
+									onMouseLeave={this.handleMouseOut}
+									onClick={this.handleClick}
+									style={style.button}
+									justify="flex-start"
+									className={this.state.hover ? 'hover' : 'hoverOut'}
+								/>
 					}
 				</TransitionGroup>
 			</Flex>
@@ -76,17 +89,26 @@ class CallToAction extends React.Component {
 }
 
 class Collapsed extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			complete: false
+		}
+	}
 	componentWillEnter (callback) {
-		console.log('hit componentWillEnter');
     const el = findDOMNode(this);
-    TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: callback});
+    TweenMax.fromTo(el, 0.3, {opacity: 0}, {opacity: 1, onComplete: callback});
   }
 
 	componentWillLeave (callback) {
-		console.log('hit componentWillLEave');
     const el = findDOMNode(this);
-    TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0, onComplete: callback});
+    TweenMax.fromTo(el, 0.0, {opacity: 1}, {opacity: 0, onComplete: callback});
   }
+	handleClick() {
+		this.setState({
+			complete: true,
+		})
+	}
 
 	render() {
 		return (
@@ -99,8 +121,63 @@ class Collapsed extends React.Component {
 	}
 }
 
-let expanded = (...props) => {
-	return null;
+class Expanded extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			hover: false
+		}
+	}
+
+	handleMouseIn() {
+		this.setState({hover:true})
+	}
+
+	handleMouseOut() {
+		this.setState({hover:false})
+	}
+
+	componentWillEnter (callback) {
+		const el = findDOMNode(this);
+		TweenMax.fromTo(el, 0.3, {opacity: 0}, {opacity: 1, onComplete: callback});
+	}
+
+	componentWillLeave (callback) {
+		const el = findDOMNode(this);
+		TweenMax.fromTo(el, 0.0, {opacity: 1}, {opacity: 0, onComplete: callback});
+	}
+	render() {
+		return (
+			<Box column {...this.props}>
+				<Flex row
+					justify="space-between">
+					<h4>I'm interested in...</h4>
+					<FontAwesome
+						inverse={true}
+						size="1x"
+						name="fa-times"
+						onClick={this.props.onClose}
+						onMouseEnter={this.handleMouseIn}
+						onMouseLeave={this.handleMouseOut}
+						style={{cursor:'pointer'}}
+					>{"\uf00d"}</FontAwesome>
+				</Flex>
+				<Flex column>
+					<input type="text" placeholder="email" />
+					<Flex row>
+						{['dancing', 'cleaning', 'child care'].map(
+							ele =>
+								<span>
+									<input onClick={this.handleClick} key={ele} type="checkbox" />
+									{ele}
+								</span>
+							)}
+					</Flex>
+					<button type="button" >Send</button>
+				</Flex>
+			</Box>);
+
+	}
 }
 
 export default CallToAction;
